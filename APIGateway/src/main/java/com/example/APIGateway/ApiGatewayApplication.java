db22d2a81d9a819a103d892e631df0e63e27c9d6
LeydiRiv <leydi.riv1@gmail.com>
2024-10-24 14:56:29 -0600

    Ticket#1: Add CORS for security and allow requests from the frontend

--- APIGateway/src/main/java/com/example/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/example/APIGateway/ApiGatewayApplication.java
@@ -2,6 +2,11 @@ package com.example.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +15,19 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+//CORS
+	@Bean
+	public CorsWebFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+//		config.addAllowedOrigin("http://127.0.0.1:5500"); //Allow requests from fronted
+		config.addAllowedOrigin("http://localhost:4200"); // //Allow requests from fronted
+		config.addAllowedHeader("*"); // Allow all headers
+		config.addAllowedMethod("*"); // Allow all methods
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config); // Allow all paths
+
+		return new CorsWebFilter(source);
+	}
 }
